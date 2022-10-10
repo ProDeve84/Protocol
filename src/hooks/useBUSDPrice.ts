dc9ce0ebba0e0a82329effdc2d40be90476978d2
ProDeve84 <114754891+ProDeve84@users.noreply.github.com>
2022-10-10 21:33:03 +0200

    Update useBUSDPrice.ts

--- src/hooks/useBUSDPrice.ts
+++ src/hooks/useBUSDPrice.ts
@@ -1,7 +1,7 @@
 import { ChainId, Currency, currencyEquals, JSBI, Price, WETH } from '@pancakeswap/sdk'
 import { useMemo } from 'react'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
-import { BUSD, BLOVELY, WBNB } from '../config/constants/tokens'
+import { BUSD, BPET, WBNB } from '../config/constants/tokens'
 import { PairState, usePairs } from './usePairs'
 import { wrappedCurrency } from '../utils/wrappedCurrency'
 
